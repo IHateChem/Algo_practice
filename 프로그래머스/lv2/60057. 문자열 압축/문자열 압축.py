082a6f55e89fbc41f94cccbc2e612929d9c18772
IHateChem <syleelsw@gmail.com>
2022-09-28 14:56:29 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,28 +1,3 @@
-import math
 def solution(s):
-    n = len(s)
-    answer = n
-    eps = 10**-10
-    k = 1
-    while k <= n //2:
-        tempanswer = n
-        pnt = 0
-        tsh = 10
-        succ = 0
-        while pnt + 2*k <= n:
-            if s[pnt:pnt+k] == s[pnt+k:pnt+2*k]:
-                if succ:
-                    tempanswer -= k
-                else:
-                    tempanswer -= k- 1
-                succ += 1
-                if succ == tsh-1:
-                    tempanswer += 1
-                    tsh *= 10
-            else:
-                succ = 0
-                tsh = 10
-            pnt += k 
-        answer = min(answer, tempanswer)
-        k += 1
+    answer = 0
     return answer
