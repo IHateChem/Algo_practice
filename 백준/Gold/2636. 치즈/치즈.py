2ad6ff7d669ec1b7c1ad8c9ad03a8b7e7c837d17
IHateChem <syleelsw@gmail.com>
2022-11-30 12:52:48 +0900

    [Gold IV] Title: 치즈, Time: 136 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -4,6 +4,12 @@ N, M= map(int, input().split())
 MAP = []; t = 0; num = -1; flag = True
 for _ in range(N):
     MAP.append(list(map(int, input().split())))
+def check(i,j,t,MAP,visited,stack):
+    if MAP[i][j] == 1:
+        MAP[i][j] = "C"; t += 1
+    elif MAP[i][j] == 0 and not visited[i][j]:
+        visited[i][j] = 1; stack.append((i, j))
+    return t
 while flag:
     past = t; t = 0;  flag = False
     stack = [(0, 0)]
@@ -11,30 +17,10 @@ while flag:
     while stack:
         i, j = stack.pop()
         if MAP[i][j]==0:
-            if i != N-1:
-                if MAP[i+1][j] == 1:
-                    MAP[i+1][j] = "C"
-                    t += 1
-                elif MAP[i+1][j] == 0 and not visited[i+1][j]:
-                    visited[i+1][j] = 1; stack.append((i+1, j))
-            if j !=M-1:
-                if MAP[i][j+1] == 1:
-                    MAP[i][j+1] = "C"
-                    t += 1
-                elif MAP[i][j+1] == 0 and not visited[i][j+1]:
-                    visited[i][j+1] = 1; stack.append((i, j+1))
-            if i !=0:
-                if MAP[i-1][j] == 1:
-                    MAP[i-1][j] = "C"
-                    t += 1
-                elif MAP[i-1][j] == 0 and not visited[i-1][j]:
-                    visited[i-1][j] = 1; stack.append((i-1, j))
-            if j !=0:
-                if MAP[i][j-1] == 1:
-                    MAP[i][j-1] = "C"
-                    t += 1
-                elif MAP[i][j-1] == 0 and not visited[i][j-1]:
-                    visited[i][j-1] = 1; stack.append((i, j-1))
+            if i != N-1: t = check(i+1,j,t,MAP,visited,stack)
+            if j !=M-1: t = check(i,j+1,t,MAP,visited,stack)
+            if i !=0: t = check(i-1,j,t,MAP,visited,stack)
+            if j !=0: t = check(i,j-1,t,MAP,visited,stack)
     for i in range(N):
         for j in range(M):
             if MAP[i][j]: flag = True
