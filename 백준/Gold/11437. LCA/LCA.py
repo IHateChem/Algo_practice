4fc8135fef00b4c7c122c89b2735023ba55d51c0
IHateChem <syleelsw@gmail.com>
2023-01-01 11:49:20 +0900

    [Gold III] Title: LCA, Time: 352 ms, Memory: 288768 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.py
+++ 백준/Gold/11437. LCA/LCA.py
@@ -1,48 +1,45 @@
 import sys
+import math
 input = sys.stdin.readline
+sys.setrecursionlimit(100000)
 N = int(input())
-treedept = {}
-tree = [[] for _ in range(N+1)]
-notin = []
-treedept[1] = 1
-for i in range(N-1):
-    u, v = map(int, input().split())
-    tree[v].append(u)
-    tree[u].append(v)
+graph = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+M = 22
+lca_table = [ [0 for __ in range(M)] for _ in range(N+1)]
+level = [0] * (N+1)
+visited = [0] * (N+1)
+def dfs(node, p_node, lv):
+    lca_table[node][0] = p_node
+    visited[node] = 1
+    level[node] = lv
+    for i in graph[node]:
+        if visited[i]: continue
+        dfs(i, node, lv + 1)
 
-visited = [0] *(N+1)
-parent = [0] *(N+1)
-stack = [(1,0)]
-dept = 1
-tempset = []
-while stack:
-    t , p= stack.pop()
-    visited[t] = dept
-    parent[t] = p
-    for next in tree[t]:
-        if not visited[next]:
-            tempset.append((next, t))
-    if not stack:
-        dept += 1
-        stack = tempset
-        tempset = []
-M = int(input())
-def getlca(u,v):
-    dept_u = visited[u]
-    dept_v = visited[v]
-    flag = dept_u < dept_v
-    if flag: 
-        for i in range(dept_v-dept_u):  v = parent[v]
-    else:
-        for i in range(dept_u-dept_v):  u = parent[u]
-    while 1:
-        if u == v:
-            break
-        v = parent[v]
-        u = parent[u]
-    return u
-a = []
-for _ in range(M):
-    u, v = map(int, input().split())
-    a.append(str(getlca(u,v)))
-print("\n".join(a))
+
+def MKlca():
+    dfs(1,0, 1)
+    for i in range(1, M):
+        for j in range(1, N+1):
+            lca_table[j][i] = lca_table[lca_table[j][i-1]][i-1]
+def getlca(a, b):
+    if level[a]> level[b]:a,b=b,a
+    for i in range(20,-1,-1):
+        if level[b]-level[a]>=2**i:
+            b=lca_table[b][i]
+    if a==b:return a
+    for i in range(20,-1,-1):
+        if lca_table[a][i]!=lca_table[b][i]:
+            a,b=lca_table[a][i],lca_table[b][i]
+    return lca_table[a][0]
+K = int(input())
+MKlca()
+answer = []
+for _ in range(K):
+    a, b = map(int, input().split())
+    print(getlca(a,b))
+#print("\n".join(answer))
