9102d72d733962510e7ab5c9969d95b384dfd3b5
IHateChem <syleelsw@gmail.com>
2023-02-11 17:54:44 +0900

    [Gold I] Title: 모노디지털 표현, Time: 144 ms, Memory: 31768 KB -BaekjoonHub

--- 백준/Gold/2287. 모노디지털 표현/모노디지털 표현.py
+++ 백준/Gold/2287. 모노디지털 표현/모노디지털 표현.py
@@ -1,4 +1,3 @@
-from collections import defaultdict as dd
 import sys
 input = sys.stdin.readline
 def get_pair(n):
@@ -6,40 +5,25 @@ def get_pair(n):
     for i in range(1, n//2+1):
         ret.append((i, n-i))
     return ret
-def get_possible(n1, n2, n_set, nums):
+def get_possible(n1, n2, n_set):
     n = n1+n2
     for i in n_set[n1]:
         for j in n_set[n2]:
-            if not nums[i+j]:
-                nums[i+j] = 1
-                n_set[n].add(i+j)
-            if not nums[i*j]:
-                nums[i*j] = 1
-                n_set[n].add(i*j)
-            if not nums[abs(i-j)]:
-                nums[abs(i-j)] = 1
-                n_set[n].add(abs(i-j))
-            if not nums[-abs(i-j)]:
-                nums[-abs(i-j)] = 1
-                n_set[n].add(-abs(i-j))
-            if j and not nums[i//j]:
-                nums[i//j] = 1
-                n_set[n].add(i//j)
-            if i and not nums[j//i]:
-                nums[j//i]= 1
-                n_set[n].add(j//i)
+            n_set[n].add(i+j)
+            n_set[n].add(i*j)
+            n_set[n].add(abs(i-j))
+            n_set[n].add(-abs(i-j))
+            if j: n_set[n].add(i//j)
+            if i: n_set[n].add(j//i)
     
 def solution(N, number):
-    numbers = dd(int)
-    numbers[N] = 1
     nums_set = [set() for _ in range(9)]
     nums_set[1].add(N)
     if N == number: return 1
     for i in range(2, 9):
         nums_set[i].add(int(str(N)*i))
-        numbers[int(str(N)*i)]=1
         for a, b in get_pair(i):
-            get_possible(a,b,nums_set,numbers)
+            get_possible(a,b,nums_set)
         if number in nums_set[i]: return i
     return -1
 K = int(input())
