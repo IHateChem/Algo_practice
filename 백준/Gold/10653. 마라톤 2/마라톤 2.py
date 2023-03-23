4f0f07b57d4d7257907abaacf748d38c05ced425
IHateChem <syleelsw@gmail.com>
2023-03-23 11:29:26 +0900

    [Gold III] Title: 마라톤 2, Time: 2168 ms, Memory: 117932 KB -BaekjoonHub

--- 백준/Gold/10653. 마라톤 2/마라톤 2.py
+++ 백준/Gold/10653. 마라톤 2/마라톤 2.py
@@ -2,8 +2,9 @@ import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
 checkpoints = [tuple(map(int, input().split())) for _ in " "*N]
-def getdist(x,y, r,s):return abs(x-r)+abs(y-s)
-INF = 1e7
+#def getdist(x,y, r,s):return abs(x-r)+abs(y-s)
+getdist = lambda x,y,r,s: abs(x-r)+abs(y-s)
+INF = 1000000
 dp = [[INF]*(K+1) for _ in " "*N]
 dp[0][0] = 0
 p = checkpoints[0]
@@ -12,8 +13,9 @@ for i in range(N):
     for j in range(K+1):
         dp[i][j] = min(dp[i][j], dp[i-1][j] + d)
     for j in range(K):
-        for k in range(1, K+1-j):
-            if i + k + 1 >= N: break
+        k = 1
+        while k +i+1 < N and k < K+1-j:
             dp[i+k+1][j+k] = min(dp[i+k+1][k+j], dp[i][j] +getdist(*checkpoints[i], *checkpoints[i+k+1]))
+            k += 1
     p = checkpoints[i]
 print(min(dp[-1]))
