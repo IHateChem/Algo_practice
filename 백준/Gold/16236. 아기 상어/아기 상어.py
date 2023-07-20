a09d9b64e4c345267039f76c385daec11750b1d9
IHateChem <syleelsw@gmail.com>
2023-07-20 09:53:15 +0900

    [Gold III] Title: 아기 상어, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,69 +1,42 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-n = int(input().strip())
-
-graph = []
-levelup = [0,0,2,5,9,14,20,2**30]
-eat = 0
-size = 2
-totdistance = 0
-def BFS( pos):
-    global graph
-    x = pos[0]
-    y = pos[1]
-    visited = [[0] * n for _ in range(n)]
-    visited[x][y] = 1
-    q = deque()
-    q.append((x,y))
-    t = []
-    distance = 0
-    while q:
-        x,  y = q.popleft()
-        global size
-        if graph[x][y] and graph[x][y] < size:
-            global eat
-            graph[x][y] = 0
-            eat += 1
-            if eat >= levelup[size]:
-                size += 1
-            global position
-            position = (x, y)
-            return distance
-        if x - 1 >= 0 and graph[x-1][y] <= size and not visited[x-1][y]:
-            visited[x-1][y] = 1
-            t.append((x-1,y))
-        if y - 1 >= 0 and graph[x][y-1] <= size and not visited[x][y-1]:
-            visited[x][y-1] = 1
-            t.append((x,y-1))
-        if y + 1 < n and graph[x][y+1] <= size and not visited[x][y+1]:
-            t.append((x,y+1))
-            visited[x][y+1] = 1
-        if x + 1 < n and graph[x+1][y] <= size and not visited[x+1][y]:
-            visited[x+1][y] = 1
-            t.append((x+1,y))
-        if not q:
-            if t:
-                t.sort()
-                q = deque(t)
-                distance += 1
-                t = []
-    return 0
-
-for i in range(n):
-    s = list(map(int, input().strip().split()))
-    try:
-        y = s.index(9)
-        position = [i,y]
-        s[y] = 0
-    except:
-        pass
-    graph.append(s)  
-state = 1
-while state:
-    state=  BFS(position)
-    if not state:
-        print(totdistance)
-        break
+N=int(input())
+MAP=[list(map(int, input().split())) for _ in range(N)]
+for i  in range(N):
+    for j in range(N):
+        if MAP[i][j] == 9:
+            x, y = i,j
+            MAP[x][y] = 0
+q = [(x,y)]; food = []; next=set(); level=2; exp=0; answer=0;history =set();count=0
+def levelup(level, exp):
+    exp += 1
+    if level == exp:
+        exp = 0
+        level += 1
+    return level, exp
+canMove = lambda  x,y,dx,dy: 0<=x+dx<N and 0<=y+dy<N and MAP[x+dx][y+dy] <= level and not (x+dx,y+dy) in history
+canEat = lambda x,y: MAP[x][y] < level and MAP[x][y]
+while q:
+    x, y = q.pop()
+    history.add((x,y))
+    for dx, dy in ((1,0),(-1,0),(0,1),(0,-1)):
+        if not canMove(x,y,dx,dy): continue
+        if canEat(x+dx,y+dy):
+            food.append((x+dx, y+dy))
+        next.add((x+dx, y+dy))
+    if q: continue
+    if food:
+        x,y = sorted(food)[0]
+        level, exp = levelup(level, exp)
+        answer += count + 1
+        MAP[x][y] = 0
+        q = [(x,y)]
+        next = set()
+        history = set()
+        food = []
+        count = 0
     else:
-        totdistance += state
+        q = list(next)
+        next = set()
+        count += 1
+print(answer)
