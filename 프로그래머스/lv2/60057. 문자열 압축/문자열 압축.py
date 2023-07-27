2e83e276016098b46aecf05c952b55cac309bce6
IHateChem <syleelsw@gmail.com>
2023-07-27 09:39:32 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,3 +1,23 @@
 def solution(s):
-    answer = 0
+    answer = 1000
+    if(len(s) == 1): return 1
+    p = ""
+    for unit in range(1, len(s)//2+1):
+        pressed = []
+        pressedlen = []
+        for idx in range(0, len(s), unit):
+            if s[idx:idx+unit] != p:
+                p = s[idx:idx+unit]
+                pressed.append(p)
+                pressedlen.append(1)
+            else:
+                pressedlen[-1] += 1
+        merge = []
+        for i in range(len(pressed)):
+            if pressedlen[i] == 1:
+                merge.append(pressed[i])
+            else:
+                merge.append(str(pressedlen[i]) + pressed[i])
+        t = "".join(merge)
+        answer = min(len(t), answer)
     return answer
