f71762014a9d78141b1f1864782ae98dc96ae3f4
Western (SeokWoo Lee ) <syleelsw@gmail.com>
2024-07-05 23:44:50 +0900

    [level 3] Title: 광고 삽입, Time: 81.32 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -1,37 +1,33 @@
+def time_parser(time: str):
+    HH, MM, SS = time.split(":")
+    return int(SS) + 60*int(MM) + 3600*int(HH)
+def log2time(log:str):
+    start_time, end_time = log.split("-")
+    st = time_parser(start_time)
+    et = time_parser(end_time)
+    return st, et
+def time2str(time: int):
+    SS = time % 60
+    HH = time // 3600
+    MM = (time // 60) % 60
+    return f'{HH:0>2}:{MM:0>2}:{SS:0>2}'
 def solution(play_time, adv_time, logs):
-    answer = ''
-    def toIndex(t):
-        h,m,s= map(int, map(str.lstrip,t.split(":")))
-        return h*3600+m*60+s
-    def toTime(t):
-        h = t // 3600
-        m = (t-h*3600) // 60
-        s = t % 60
-        if h < 10: h = "0" + str(h)
-        if m < 10: m = "0" + str(m)
-        if s < 10: s = "0" + str(s)
-        return str(h) + ":"+ str(m)+ ":" + str(s)
-    times = [0]*100*3600
+    times = [0]*(3600*100+2)
+    adv_time = time_parser(adv_time)
     for log in logs:
-        s,e = log.split("-")
-        start = toIndex(s)
-        end= toIndex(e)
-        times[start]+= 1
-        times[end] -= 1
-    idx=0
-    window=0
-    windowSize = toIndex(adv_time)
-    for i in range(1, 100*3600):
-        times[i] += times[i-1]
-    for i in range(windowSize):
-        window += times[i]
-    answer = window
-    for i in range(windowSize,100*3600):
-        window -= times[i-windowSize] - times[i]
-        if answer < window:
-            answer =  window
-            idx = i-windowSize+1
-    
-        
-        
-    return toTime(idx)
+        start_time, end_time = log2time(log)
+        times[start_time] += 1
+        times[end_time] -= 1
+    for i in range(360000):
+        times[i+1] += times[i]
+    window = sum(times[:adv_time])
+    max_idx = 0
+    max_sum = window
+    idx = adv_time
+    while idx < (3600*100+1):
+        window += - times[idx - adv_time] + times[idx]
+        if window > max_sum:
+            max_sum = window
+            max_idx = idx - adv_time + 1
+        idx += 1
+    return time2str(max_idx)
