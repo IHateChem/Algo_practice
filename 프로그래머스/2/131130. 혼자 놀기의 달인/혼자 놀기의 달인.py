ee8090f6e4639ba2f55c3e2cb65300e7c19a5b49
Western (SeokWoo Lee ) <syleelsw@gmail.com>
2024-07-06 22:24:35 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -1,15 +1,3 @@
 def solution(cards):
-    visited = set()
-    group_size = []
-    def dfs(a, group_len):
-        visited.add(a)
-        if cards[a] -1 in visited:
-            return group_len
-        return dfs(cards[a]-1, group_len+1)
     answer = 0
-    for i in range(len(cards)):
-        if i in visited: continue
-        group_size.append(dfs(i, 1))
-    if len(group_size) == 1: return 0
-    group_size.sort()
-    return group_size[-1] * group_size[-2]
+    return answer
